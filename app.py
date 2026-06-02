e60d624bdba7a45ec4ae6d67197924d64cc926fe
Saurabh Aryal <saurabharyal10@gmail.com>
2026-06-02 08:56:34 +0545

    Initial Commit

--- app.py
+++ app.py
@@ -4,8 +4,11 @@ import matplotlib.pyplot as plt
 import streamlit as st
 
 from sqlalchemy import create_engine
+import os
+from dotenv import load_dotenv
 
-DATABASE_URL = f'postgresql://readonly_student:StudentRead123!@db.tyxjmbptftftcqgozyfc.supabase.co:5432/postgres'
+load_dotenv()
+DATABASE_URL = f"postgresql://{os.getenv('DB_USERNAME')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
 engine = create_engine(DATABASE_URL)
 query = """ SELECT
 	t."name", t.album_id, a.title AS album_title, a2."name" AS artist_name,
@@ -25,23 +28,32 @@ query = """ SELECT
  
 df = pd.read_sql(query, engine)
 
-df_count_by_genre = df.groupby('genre_name').agg(num_tracks = ('track_id', 'count')).reset_index()
-print(df_count_by_genre)
+# create a barplot with the number of artists (Top 50 artists)
+# Create a histogram that shows the distribution of song(track)length in minutes (fields-milliseconds)
 
-fig, ax = plt.subplots()
-sns.barplot(data=df_count_by_genre.sort_values('num_tracks', ascending=False), y="genre_name", x="num_tracks", ax=ax)
+df['minutes'] = df['milliseconds'] / 60000
+
+selected_genre = st.multiselect('Select the genre', df['genre_name'].unique(), default= df['genre_name'].unique())
+filtered_df = df[df['genre_name'].isin(selected_genre)]
+
+track_count_by_genre = filtered_df.groupby('genre_name').agg(num_tracks = ('track_id', 'count')).reset_index()
+# print(track_count_by_genre)
+fig,ax = plt.subplots(figsize=(10,6))
+sns.barplot(data=track_count_by_genre.sort_values('num_tracks', ascending=False), y="genre_name", x="num_tracks", ax=ax)
 st.pyplot(fig)
 
+track_count_by_artist = filtered_df.groupby('artist_name').agg(num_tracks = ('track_id', 'count')).reset_index()
+fig1, ax1 = plt.subplots(figsize=(10,6))
+sns.barplot(data=track_count_by_artist.sort_values('num_tracks', ascending= False ).head(25) , x = 'num_tracks' , y = 'artist_name', ax= ax1)
+st.pyplot(fig1)
+
+
+# Histogram of song length in minutes (fields-milliseconds)
+fig2, ax2 = plt.subplots(figsize=(10,6))
+sns.histplot(data= filtered_df, x = 'minutes', binwidth=1, ax = ax2)
+st.pyplot(fig2)
+
+# Filtered Table by Genre
 artist = st.selectbox('Select artist', df['artist_name'].unique())
 filtered_df = df[df['artist_name'] == artist]
 st.dataframe(filtered_df[['name', 'album_title', 'artist_name', 'genre_name']])
-
-
-# st.columns
-# tukeys law
-	
- 
-#  add more filters
-#  add buttons
-#  add slides
-#  show more graphs
